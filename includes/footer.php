4d9628f858ac9e239e235e5903008166dc579c0f
anushanram <hpanushan@gmail.com>
2019-05-26 14:49:52 +0530

    Created simple webpage

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+<footer>
+		<p> Comapy Name | Address | Tel : 0774877878</p>
+	</footer>
+
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+	<header>
+		<h2>Anushan</h2>
+	</header>
+
+	<nav>
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="about.php">About</a></li>
+			<li><a href="products.php">Products</a></li>
+			<li><a href="contact.php">Contact Us</a></li>
+		</ul>
+	</nav>
